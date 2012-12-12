42fb54de7df11ef62f15cd5a90df0292da008581
Andreas Lind Petersen <andreas@one.com>
2012-12-12 23:28:38 +0100

    Better test descriptions.

--- test/compiless.js
+++ test/compiless.js
@@ -5,7 +5,7 @@ var express = require('express'),
     expect = require('expect.js'),
     compiless = require('../lib/compiless');
 
-describe('test server', function () {
+describe('test server with compiless', function () {
     var root = Path.resolve(__dirname, 'root'),
         // Pick a random TCP port above 10000 (.listen(0) doesn't work anymore?)
         portNumber = 10000 + Math.floor(55536 * Math.random()),
@@ -23,7 +23,7 @@ describe('test server', function () {
         server.close();
     });
 
-    it('request for non-less file', function (done) {
+    it('should not mess with request for non-less file', function (done) {
         request(baseUrl + '/something.txt', passError(done, function (response, body) {
             expect(body).to.equal("foo\n");
             expect(response.headers['content-type']).to.equal('text/plain; charset=UTF-8');
@@ -31,7 +31,7 @@ describe('test server', function () {
         }));
     });
 
-    it('request for less file with @import', function (done) {
+    it('should compile less file with @import to css with .compilessinclude rules first', function (done) {
         request(baseUrl + '/stylesheet.less', passError(done, function (response, body) {
             expect(body).to.equal('.compilessinclude {background-image: url(imports/a.less); display: none;}\nbody {\n  width: 100%;\n}\n#foo #bar {\n  color: red;\n}\n');
             expect(response.headers['content-type']).to.equal('text/css');
@@ -39,7 +39,7 @@ describe('test server', function () {
         }));
     });
 
-    it('request for less file with syntax error', function (done) {
+    it('should render less file that has a syntax error with the error message as the first thing in the output, wrapped in a body:before rule', function (done) {
         request(baseUrl + '/syntaxerror.less', passError(done, function (response, body) {
             expect(response.statusCode).to.equal(200);
             expect(response.headers['content-type']).to.equal('text/css');
@@ -48,7 +48,7 @@ describe('test server', function () {
         }));
     });
 
-    it('request for less file with @import error', function (done) {
+    it('should render less file that has an @import error with the error message as the first thing in the output, wrapped in a body:before rule', function (done) {
         request(baseUrl + '/importerror.less', passError(done, function (response, body) {
             expect(response.statusCode).to.equal(200);
             expect(response.headers['content-type']).to.equal('text/css');
@@ -57,7 +57,7 @@ describe('test server', function () {
         }));
     });
 
-    it('request for less file with second level @import error', function (done) {
+    it('should render less file that has an second level @import error with the error message as the first thing in the output, wrapped in a body:before rule', function (done) {
         request(baseUrl + '/secondlevelimporterror.less', passError(done, function (response, body) {
             expect(response.statusCode).to.equal(200);
             expect(response.headers['content-type']).to.equal('text/css');
